e05d8c68a0eda4fdb2f6e1db4e440f1e1b345b6f
ak1303 <13abhi03@gmail.com>
2023-10-01 19:05:33 +0530

    test

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,30 +4,54 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
+    private int bill;
 
+    private boolean extraCheeseAdded;
+    private boolean extraToppingsAdded;
+    private boolean taken = false;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg) this.price = 300;
+        else this.price =400;
+        this.extraCheeseAdded=false;
+        this.extraToppingsAdded = false;
+        this.bill=this.price;
     }
 
     public int getPrice(){
-        return this.price;
+        return price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        if(!extraCheeseAdded){
+            this.bill += 80;
+            extraCheeseAdded =true;
+        }
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!extraToppingsAdded){
+            if(isVeg)this.bill+=70;
+            else this.bill += 120;
+            extraToppingsAdded = true;
+        }
+
     }
 
-    public void addTakeaway(){
+    public final void addTakeaway(){
         // your code goes here
+        if(!taken){
+            this.bill+=20;
+            taken = true;
+        }
+
     }
 
-    public String getBill(){
+    public int getBill(){
         // your code goes here
         return this.bill;
     }
