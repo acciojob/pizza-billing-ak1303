d89b95199a0dcfa6c745d970a0cb0648d30d90cd
ak1303 <13abhi03@gmail.com>
2023-10-25 23:17:05 +0530

    test

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -8,15 +8,4 @@ public class DeluxePizza extends Pizza {
         addExtraCheese();
         addExtraToppings();
     }
-
-    @Override
-    public String getBill(){
-        // your code goes here
-        String s="";
-        s +="Base Price Of The Pizza: "+this.price+"\n";
-        if(taken)
-            s+="Paperbag Added: 20\n";
-        s+="Total Price: "+this.bill+"\n";
-        return s;
-    }
 }
